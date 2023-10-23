2a2b8f76ba929e68c9b61284b3f2303ee42ccf18
vira09deswita <148793006+vira09deswita@users.noreply.github.com>
2023-10-23 22:49:29 +0700

    Add files via upload

---
+++ latihan4.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+	<meta charset="UTF-8">
+	<title>Modul 2 -Latihan 1</title>
+</head>
+<body>
+	<table border= "1" cellpadding="3" cellspacing="0">
+		<tr>
+			<th>Kolom 1</th>
+			<th>Kolom 2</th>
+			<th>Kolom 3</th>
+			<th>Kolom 4</th>
+			<th>Kolom 5</th>
+		</tr>
+
+		<!-- TULIS SCRIPT PHP DIBAWAH BARIS INI -->
+	<?php
+	$rows = 15;
+	$cols = 5;
+	echo "</tr>";
+	for ($i = 1; $i <= $rows; $i++) {
+		echo "<tr>";
+		for ($j = 1; $j <= $cols; $j++) {
+		echo "<td>Baris $i, kolom $j</td>";
+		}
+		echo "</tr>";
+
+	}
+	echo "</table>";
+	?>
+	
+	</table>
+</body>
+</html>
+
